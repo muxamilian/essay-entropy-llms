bd0d1a1840e6d820d880589db3415efda8242175
Maximilian Bachl <maximilian.bachl@gmail.com>
2024-02-27 20:26:16 +0100

    plotting code

--- plot_all.py
+++ plot_all.py
@@ -43,12 +43,27 @@ plt.ylim(-.25, 0)
 # Set logarithmic scale for x-axis with custom tick labels
 tick_values = list(params.values())
 tick_labels = [f"{net}, {params[net]/1e6:.0f}M" for net in params]
-tick_labels[1] = '\n'+tick_labels[1]
+tick_values, tick_labels = zip(*sorted(zip(tick_values, tick_labels), key=lambda x: x[0]))
 plt.xscale('log')
 plt.xticks(tick_values, labels=tick_labels, rotation=45, ha="right")
+
 plt.grid(True, ls="--", linewidth=0.5, color='gray', alpha=0.5)
 plt.tight_layout()
 
+# Get the current axes
+ax = plt.gca()
+
+original_tick = ax.get_xticklabels()[1]
+original_x, original_y = original_tick.get_position()
+
+# Hide the original second tick label
+original_tick.set_visible(False)
+
+# Manually add a new text label for the second tick, adjust the position as needed
+new_x_position = original_x + 10000000  # Adjust this value as needed
+# ax.text(new_x_position, original_y, 'B', ha='right', rotation=45)
+ax.text(new_x_position, -0.298, tick_labels[1], ha='right', rotation=45)
+
 # Display the plot
 plt.savefig('plots/all.pdf')
-plt.show()
+# plt.show()
